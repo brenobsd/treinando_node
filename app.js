0f9686a9b7968d882168be44e6f131d2805e1ed6
brenobsd <bre.bsilva10@gmail.com>
2023-06-22 15:14:39 -0300

    testando o npm

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
+//Incluindo uma biblioteca
 const http = require('http');
+const url = require('url');
+const queryString = require('query-string');
 
-const hostname = '127.0.0.1';
+//Definição de endereço/URL
+const hostname = '127.0.0.1';//localhost 
 const port = 3000;
 
+//Implementação da regra de negócio
 const server = http.createServer((req, res) => {
+  
+//Pegar a pergunta na url
+console.log(req.url);
+const params = queryString.parse(url.parse(req.url),true).search;
+console.log(params);
+//Verificar a pergunta e escolher uma resposta
+//Retornar a resposta escolhida
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('brocha');
+  res.end('breno dev');
 });
 
+//Execução
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-gi
+});
