c213f23f2f2ccb6a304bdd67cfd0268d9899b031
brenobsd <bre.bsilva10@gmail.com>
2023-06-22 13:58:05 -0300

    app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('brocha');
+  res.end('Hello world');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-gi
+});
